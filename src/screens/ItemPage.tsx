1ea222c30b6304602cade3d4f1f1f605d74ee1cc
James Nicholas <wastrilith@gmail.com>
2025-05-02 16:40:50 -0700

    chore: wip

--- src/screens/ItemPage.tsx
+++ src/screens/ItemPage.tsx
@@ -8,6 +8,8 @@ import {
   PageButtonContainer,
   PageContainer,
   PageHeader,
+  StyledLeftBottomButton,
+  ConfigLink,
 } from '../components/styles';
 import { Icon } from '../components/Icon';
 import { useAuthState } from 'react-firebase-hooks/auth';
@@ -15,30 +17,50 @@ import { auth } from '../config/firebaseConfig';
 import { Modal } from '../components/Modal';
 import { sendEventToAWS } from '../config/awsConfig';
 import { MetricsNavButton } from '../components/MetricsNavButton';
+import { createNamedEvent, deleteEvent, fetchEvents, updateEvents } from '../utils/aws.utils';
+import { AwsNamedEvent } from '../utils/types';
+import { useEditMode } from '../components/EditStateProvider';
+import { Reorder } from 'framer-motion';
 
 const ItemPage = () => {
   const { name } = useParams() as { name: string };
   const navigate = useNavigate();
   const [items, setItems] = useState<string[]>([]);
+  const [events, setEvents] = useState<AwsNamedEvent[]>([]);
+  const [newEventName, setNewEventName] = useState<string>('');
   const [isSent, setIsSent] = useState(false);
   const [message, setMessage] = useState('');
-  const [isModalOpen, setIsModalOpen] = useState<boolean>(false);
+  const [isLogEventModalOpen, setIsLogEventModalOpen] = useState<boolean>(false);
   const [item, setItem] = useState<string | null>(null);
   const [text, setText] = useState<string>('');
+
+  const [isAddEventNameModalOpen, setIsAddEventNameModalOpen] = useState<boolean>(false);
+  const [isDeleteConfirmModalOpen, setIsDeleteConfirmModalOpen] = useState<boolean>(false);
+  const [eventToDelete, setEventToDelete] = useState<AwsNamedEvent | null>(null);
+  const { editMode, setEditMode } = useEditMode();
+
+  const loadEvents = () => {
+    fetchEvents(user?.uid ?? 'unknown', name).then((events) => {
+      console.debug('Fetched AWS events:', events);
+      events.sort((a: AwsNamedEvent, b: AwsNamedEvent) => a.sortOrder - b.sortOrder);
+      setEvents(events);
+    })
+  };
 
   useEffect(() => {
     const fetchedItems = fetchItems(name);
     setItems(fetchedItems);
+    loadEvents();
   }, []);
   const [user] = useAuthState(auth);
 
   const handleItemClick = (item: string) => {
     setItem(item);
-    setIsModalOpen(true);
+    setIsLogEventModalOpen(true);
   };
 
   const handleSubmit = () => {
-    setIsModalOpen(false);
+    setIsLogEventModalOpen(false);
     sendEvent(name, item as string, (user?.email ?? 'unknown') as string, text);
     setMessage(`${name} ${item}`);
 
@@ -58,6 +80,52 @@ const ItemPage = () => {
     setTimeout(() => navigate('/'), 2000);
   }
 
+
+  const handleAddEventClick = () => {
+    setIsAddEventNameModalOpen(true);
+  }
+
+  const handleDeleteEventClick = (name: AwsNamedEvent) => {
+    setEventToDelete(name);
+    setIsDeleteConfirmModalOpen(true);
+  };
+
+  const handleDeleteNameSubmit = () => {
+    if (eventToDelete !== null) {
+      deleteEvent(eventToDelete, user?.uid ?? 'unknown').then(() => {
+        loadEvents();
+      }).catch((error) => {
+        console.error('Error deleting name:', error);
+      }).finally(() => {
+        setEventToDelete(null);
+        setIsDeleteConfirmModalOpen(false);
+      });
+    };
+  }
+
+  const handleAddEventSubmit = () => {
+    if (newEventName !== null && newEventName.trim() !== '') {
+      createNamedEvent(name, newEventName, user?.uid ?? 'unknown').then(() => {
+        setIsAddEventNameModalOpen(false);
+        loadEvents();
+      })
+        .catch((error) => {
+          console.error('Error creating name:', error);
+        }).finally(() => {
+          setNewEventName('');
+          setIsAddEventNameModalOpen(false);
+        });
+    }
+  };
+
+  const handleReorder = (newOrder: AwsNamedEvent[]) => {
+    console.log('new Order:', newOrder);
+    setEvents(newOrder);
+    const payload = newOrder.map((event, index) => ({ ...event, sortOrder: index }));
+    updateEvents(payload, name, user?.uid ?? 'unknown')
+  };
+
+
   return (
     <PageContainer>
       <StyledLeftTopButton onClick={() => navigate('/')}>
@@ -68,24 +136,53 @@ const ItemPage = () => {
       <PageButtonContainer>
         {!isSent ? (
           <Fragment>
-            {items.map((item, idx) => {
-              const colorIndex = idx % 4;
-              return (
-                <Button
-                  key={item}
-                  onClick={() => handleItemClick(item)}
-                  $colorIndex={colorIndex}
-                >
-                  {item}
-                </Button>
-              );
-            })}
-          </Fragment>
-        ) : (
+            {editMode ? (<Reorder.Group axis='y' values={events} onReorder={handleReorder}>
+              {events.map((event, idx) => {
+                const colorIndex = idx % 4;
+                return (
+                  <Reorder.Item key={event.id} value={event}>
+                    <span onClick={() => handleDeleteEventClick(event)}>
+                      <Icon name="delete" size={24} fill />
+                    </span>
+                    <Button
+                      key={event.id}
+                      onClick={() => handleItemClick(event.event)}
+                      $colorIndex={colorIndex}
+                    >
+                      {event.event}
+                    </Button>
+                    <Icon name="drag_pan" size={48} fill />
+                  </Reorder.Item>
+                );
+              })}
+            </Reorder.Group>) : (
+              <Fragment>
+                {items.map((item, idx) => {
+                  const colorIndex = idx % 4;
+                  return (
+                    <Button
+                      key={item}
+                      onClick={() => handleItemClick(item)}
+                      $colorIndex={colorIndex}
+                    >
+                      {item}
+                    </Button>
+                  );
+                })}
+              </Fragment>
+            )}</Fragment>) : (
           <h1>{`Logging: ${message}`}</h1>
         )}
+        {
+          editMode && !isSent ? (<ConfigLink
+            key='add-event'
+            onClick={() => handleAddEventClick()}
+          >
+            + Add Event
+          </ConfigLink>) : null
+        }
       </PageButtonContainer>
-      <Modal isOpen={isModalOpen} onClose={() => setIsModalOpen(false)}>
+      <Modal isOpen={isLogEventModalOpen} onClose={() => setIsLogEventModalOpen(false)}>
         <h3>Comment?</h3>
         <textarea
           value={text}
@@ -95,9 +192,42 @@ const ItemPage = () => {
         <ModalActions>
           <Button $baseWidth='100%' $colorIndex={1} onClick={() => handleSubmit()}>Submit</Button>
           <Button $baseWidth='100%' $colorIndex={1} onClick={() => { setText(''); handleSubmit() }}>Skip</Button>
-          <Button $baseWidth='100%' $colorIndex={3} onClick={() => { setText(''); setIsModalOpen(false) }}>Cancel</Button>
+          <Button $baseWidth='100%' $colorIndex={3} onClick={() => { setText(''); setIsLogEventModalOpen(false) }}>Cancel</Button>
         </ModalActions>
       </Modal>
+      <Modal isOpen={isAddEventNameModalOpen} onClose={() => setIsAddEventNameModalOpen(false)}>
+        <h3>Add Event to track</h3>
+        <input type="text"
+          value={newEventName}
+          onChange={(e) => {
+            setNewEventName(e.target.value);
+
+          }}
+          onKeyDown={(e) => {
+            if (e.key === 'Enter') {
+              handleAddEventSubmit();
+            }
+          }}
+          placeholder='Name'
+        />
+        <ModalActions>
+          <Button $baseWidth='100%' $colorIndex={1} onClick={() => handleAddEventSubmit()}>Submit</Button>
+          <Button $baseWidth='100%' $colorIndex={3} onClick={() => { setNewEventName(''); setIsAddEventNameModalOpen(false) }}>Cancel</Button>
+        </ModalActions>
+      </Modal>
+      <Modal isOpen={isDeleteConfirmModalOpen} onClose={() => { setIsDeleteConfirmModalOpen(false) }}>
+        <h3>{`Confirm Deletion of ${eventToDelete?.name}`}</h3>
+        <ModalActions>
+          <Button $baseWidth='100%' $colorIndex={1} onClick={handleDeleteNameSubmit}>Confirm</Button>
+          <Button $baseWidth='100%' $colorIndex={3} onClick={() => {
+            setEventToDelete(null);
+            setIsDeleteConfirmModalOpen(false);
+          }}>Cancel</Button>
+        </ModalActions>
+      </Modal>
+      <StyledLeftBottomButton onClick={() => setEditMode(!editMode)}>
+        <Icon name="settings" size={24} fill={editMode} />
+      </StyledLeftBottomButton>
     </PageContainer>
   );
 };
--- src/utils/aws.utils.ts
+++ src/utils/aws.utils.ts
@@ -1,9 +1,30 @@
 import { invokeUrl } from './constants';
 import { AwsNamedEvent, AwsName } from './types';
 
+export const createNamedEvent = async (
+  name: string,
+  namedEvent: string,
+  user: string,
+) => {
+  const response = await fetch(`${invokeUrl}config/event`, {
+    method: 'PUT',
+    headers: {
+      'Content-Type': 'application/json',
+      'Event-User': user,
+    },
+    body: JSON.stringify({ name, event: namedEvent }),
+  });
+  if (!response.ok) {
+    throw new Error(`HTTP error! status: ${response.status}`);
+  } else {
+    console.debug('Event created in API gateway');
+  }
+  return response.json();
+};
+
 export const fetchEvents = async (user: string, name: string) => {
-  const escapaedName = encodeURIComponent(name);
-  const response = await fetch(`${invokeUrl}config/event/${escapaedName}`, {
+  const escapedName = encodeURIComponent(name);
+  const response = await fetch(`${invokeUrl}config/event/${escapedName}`, {
     method: 'GET',
     headers: {
       'Content-Type': 'application/json',
@@ -18,7 +39,11 @@ export const fetchEvents = async (user: string, name: string) => {
   return response.json();
 };
 
-export const updateEvents = async (events: AwsNamedEvent[], user: string) => {
+export const updateEvents = async (
+  events: AwsNamedEvent[],
+  name: string,
+  user: string,
+) => {
   const response = await fetch(`${invokeUrl}config/event`, {
     method: 'PUT',
     headers: {
