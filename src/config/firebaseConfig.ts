e2790eea00c6b5213689dff94b5aaba56890bca7
James Nicholas <james@01webdevelopment.com>
2026-02-18 15:39:53 -0800

    refactor: move Firebase config to environment variables

--- src/config/firebaseConfig.ts
+++ src/config/firebaseConfig.ts
@@ -5,13 +5,13 @@ import { getAuth, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: 'AIzaSyDwU9MWcnGwnOkpTrXzHR0gXIese6Qm31I',
-  authDomain: 'tracker-e928d.firebaseapp.com',
-  projectId: 'tracker-e928d',
-  storageBucket: 'tracker-e928d.firebasestorage.app',
-  messagingSenderId: '200308353165',
-  appId: '1:200308353165:web:681ba5527d76565555dac4',
-  measurementId: 'G-5YGL8HTVDH',
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
